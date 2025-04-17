24a852314fad225c437f8366342ece19435bdc88
trenpixster_quench <nizar@quen.ch>
2025-04-17 22:31:12 +0100

    Customize

--- init.lua
+++ init.lua
@@ -275,7 +275,6 @@ require('lazy').setup({
       },
     },
   },
-
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
